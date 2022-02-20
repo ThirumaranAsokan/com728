a04f4e7edf50662574401def1d73e198c40f900a
ThirumaranAsokan <Thirumaranasokan1210@gmail.com>
2022-02-20 07:08:25 +0530

    Updated code to display a longer multi-line message.

--- multiline_message.py
+++ multiline_message.py
@@ -1,3 +1,8 @@
-# Display message to the standard output
 print("System Failure Imminent!")
 print("System reboot has been initiated...")
+print()
+print("...rebooting sensory system")
+print("...rebooting sensory system")
+print("...rebooting sensory system")
+print()
+print("System online.")
