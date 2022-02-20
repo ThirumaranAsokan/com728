488373c8e30ab33a6f6293e2af43f22f2ff15bf4
ThirumaranAsokan <Thirumaranasokan1210@gmail.com>
2022-02-20 07:12:49 +0530

    Added code to display escape characters.

---
+++ escape_characters.py
@@ -0,0 +1,6 @@
+# Display escape characters
+print("\n Displays a new line")
+print("\t Displays a tab space")
+print("\\ Displays a back slash")
+print("\" Displays a double quote")
+print("\' Displays a single quote")
