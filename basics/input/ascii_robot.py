d42702835d5e58342b89bf2ec566dd073fc60fa9
ThirumaranAsokan <Thirumaranasokan1210@gmail.com>
2022-02-20 17:31:55 +0530

    Added code to display an ASCII robot with dynamic eyes.

---
+++ basics/input/ascii_robot.py
@@ -0,0 +1,13 @@
+
+
+eye=input("/|0   0|\ \n")
+print("  _____")
+print(f"/|{eye}  {eye}|\ \ ")
+print("||  _  || ")
+print("  -----")
+print("| |    | |")
+print("| /    \ |")
+print("\/ ____ \|")
+print("| |    | |")
+print("|_|    |_|")
+print(" B E E P")
