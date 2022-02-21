e651dd4f6294a42410384030c3092b36dc63ca9e
ThirumaranAsokan <Thirumaranasokan1210@gmail.com>
2022-02-22 02:22:29 +0530

    Added code to demonstrate the use of comparison operators.

---
+++ comparison_operators.py
@@ -0,0 +1,8 @@
+a=int(input("Please enter the first number\n"))
+b=int(input("Please enter the second number\n"))
+if a>b:
+    print("The second number is smallest")
+elif a==b:
+    print("Both are equal")
+else:
+    print("The first number is smallest")
