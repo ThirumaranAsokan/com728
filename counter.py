9eea5f5e6d4823f49d4b9e73f3ccb7b98bc033d7
ThirumaranAsokan <Thirumaranasokan1210@gmail.com>
2022-02-22 16:55:40 +0530

    Delete file from git respirotory

--- counter.py
+++
@@ -1,6 +0,0 @@
-n=int(input("Please enter the first whole number\n"))
-n=int(input("Please enter the second whole number\n"))
-n=int(input("Please enter the third whole number\n"))
-odd_count = 3 - n%2!=0
-even_count = 3 - n%2==0
-print(f"There were {even_count} even and {odd_count} odd numbers")0
