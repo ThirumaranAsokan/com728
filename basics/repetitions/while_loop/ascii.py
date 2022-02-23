0afadb38a415d1f5b944b8cb63a9e06b3b1c9dc7
ThirumaranAsokan <Thirumaranasokan1210@gmail.com>
2022-02-23 22:19:08 +0530

    Added code to demonstrate the use of a while loop with ascii art

---
+++ basics/repetitions/while_loop/ascii.py
@@ -0,0 +1,11 @@
+print('How many bar cables should be charged?')
+user = int(input())
+a = 0
+while a < user :
+    print("Charging:",end='')
+    a = a+1
+    print("\u2588\t"*a)
+print("The battery is fully charged.")
+
+
+
